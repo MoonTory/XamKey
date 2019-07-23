a3654c09c2b8ce9dca1f3117470b794d338118b9
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-07-22 23:46:19 -0300

    minor: Initial Keypad work

---
+++ XamKey/XamKey/Keypad/Keypad.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamKey.Keypad
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Keypad : ContentView
+    {
+        public Keypad()
+        {
+            InitializeComponent();
+        }
+
+        static class Constants
+        {
+            public static readonly Style KeypadStyles = new Style(typeof(Button));
+        }
+    }
+}
