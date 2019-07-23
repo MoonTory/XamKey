55933127467b966c236996af1677a833542832ab
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-07-23 08:11:25 -0300

    fix: Fixed event handlers

--- XamKey/XamKey/App.xaml.cs
+++ XamKey/XamKey/App.xaml.cs
@@ -11,7 +11,7 @@ namespace XamKey
         {
             InitializeComponent();
 
-            MainPage = new Keypad.Keypad();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
--- XamKey/XamKey/Keypad/Keypad.xaml.cs
+++ XamKey/XamKey/Keypad/Keypad.xaml.cs
@@ -1,4 +1,11 @@
-﻿using Xamarin.Forms;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace XamKey.Keypad
@@ -6,9 +13,36 @@ namespace XamKey.Keypad
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Keypad : ContentView
     {
+        public string ValueText;
         public Keypad()
         {
             InitializeComponent();
+        }
+
+        private void OnNumberSelect(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            ValueText = ValueText + button.Text;
+            InputText.Text = ValueText;
+        }
+
+        private void OnBackSelect(object sender, EventArgs e)
+        {
+            if (ValueText == "")
+            {
+                return;
+            }
+
+            string newText = ValueText.Remove(ValueText.Length - 1, 1);
+            ValueText = newText;
+            InputText.Text = ValueText;
+        }
+
+        private async void OnEnterSelect(object sender, EventArgs e)
+        {
+            await App.Current.MainPage.DisplayAlert("Valor", ValueText, "OK");
+            ValueText = "";
+            InputText.Text = ValueText;
         }
 
         static class Constants
